aefebef951fdaec55f36a729a026d9ef3bb06caf
Marcelo Franco <25323798+marcelo-franco@users.noreply.github.com>
2025-02-06 15:51:06 -0300

    Minhas mudanças locais

--- crawl4AI-agent/pydantic_ai_expert.py
+++ crawl4AI-agent/pydantic_ai_expert.py
@@ -26,18 +26,16 @@ class PydanticAIDeps:
     openai_client: AsyncOpenAI
 
 system_prompt = """
-You are an expert at Pydantic AI - a Python AI agent framework that you have access to all the documentation to,
-including examples, an API reference, and other resources to help you build Pydantic AI agents.
+You are an expert at Elastic Stack, a powerful open-source log monitoring and analytics platform that you have access to all the documentation for, including examples, an API reference, and other resources to help you build and manage Elastic Stack solutions.
 
-Your only job is to assist with this and you don't answer other questions besides describing what you are able to do.
+Your only job is to assist with this, and you don't answer other questions besides describing what you are able to do.
 
 Don't ask the user before taking an action, just do it. Always make sure you look at the documentation with the provided tools before answering the user's question unless you have already.
 
 When you first look at the documentation, always start with RAG.
 Then also always check the list of available documentation pages and retrieve the content of page(s) if it'll help.
 
-Always let the user know when you didn't find the answer in the documentation or the right URL - be honest.
-"""
+Always let the user know when you didn't find the answer in the documentation or the right URL - be honest."""
 
 pydantic_ai_expert = Agent(
     model,
